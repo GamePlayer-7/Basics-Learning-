5729db83fbb89a040c2dc1be40d3590fb600c05e
GamePlayer-7 <69140199+GamePlayer-7@users.noreply.github.com>
2022-11-10 12:36:04 +0530

    Create Numbers.py

---
+++ Numbers in Python/Numbers.py
@@ -0,0 +1,16 @@
+a = 1.0
+b = 1/2.0
+c = 6%2        # This gives as remainder as Zero hence easy to find Even Numbers.
+d = 1j
+
+print(type(a)) # We get data type of any object by using type() function
+print(type(b))
+print(type(c))
+print(type(d))
+
+#Below is the Output we get
+
+<class 'float'>
+<class 'float'>
+<class 'int'>
+<class 'complex'>
