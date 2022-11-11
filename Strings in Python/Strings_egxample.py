640579f46a2116d6aa8fa718ae0ba8ccacc34431
GamePlayer-7 <69140199+GamePlayer-7@users.noreply.github.com>
2022-11-11 10:54:02 +0530

    Create Strings_egxample.py

---
+++ Strings in Python/Strings_egxample.py
@@ -0,0 +1,18 @@
+string = 'Strings in Python'
+print(string)            # Prints complete string
+print(string[0])         # Prints first character of the string
+print(string[5:13])      # Prints characters starting from 3rd to 5th
+print(string[3:])        # Prints string starting from 4th character
+print(string[:7])        # Prints string starting from Begning till 6th character
+print(string * 2)        # Prints string two times
+print(string + "Easy")   # Prints concatenated string
+
+# Below will be the output
+
+Strings in Python
+S
+gs in Py
+ings in Python
+Strings
+Strings in PythonStrings in Python
+Strings in PythonEasy
