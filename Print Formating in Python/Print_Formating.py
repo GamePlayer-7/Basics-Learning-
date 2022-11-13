d95b761be6b3a63757f81790c1bb92186868c343
GamePlayer-7 <69140199+GamePlayer-7@users.noreply.github.com>
2022-11-13 13:37:43 +0530

    Create Print_Formating.py

---
+++ Print Formating in Python/Print_Formating.py
@@ -0,0 +1,9 @@
+Name = 'Sagar'
+Designation = 'Engineer'
+Role = 'Pyhton_Developer'
+Num = 4
+print("My name is {one} , I am an {two} and working as an {three} with {four} years of Experience".format(one=Name, two=Designation, three=Role, four=Num))
+
+#Below is the Output :
+
+My name is Sagar , I am an Engineer and working as an Pyhton_Developer with 4 years of Experience.
