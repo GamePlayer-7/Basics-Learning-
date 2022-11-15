a20af7da5bc17e14185662447aa3e1502ec61f5e
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2022-11-15 11:08:14 +0530

    Create Update&Add.py

---
+++ Dictionaries in Python/Update&Add.py
@@ -0,0 +1,10 @@
+dict = {'name': 'Python', 'version': 3.7}
+dict['version'] = 3.9            # Updates version to 3.9
+print(dict)
+dict['application'] = 'VS_Code'  # Adding new element with value
+print(dict)
+
+# Below is the Output :
+
+{'name': 'Python', 'version': 3.9}
+{'name': 'Python', 'version': 3.9, 'application': 'VS_Code'}
