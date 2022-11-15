3a9be207d23eb355a38116ec08d2b6885e634f9d
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2022-11-15 10:56:55 +0530

    Create Dict.py

---
+++ Dictionaries in Python/Dict.py
@@ -0,0 +1,10 @@
+dict = {'name': 'Python', 'version': 3.7}
+print(dict)             # prints entire dictionary 
+print(dict['name'])     # prints name from dictionary
+print(dict['version'])  # prints version from dictionary
+
+# Below is the output :
+
+{'name': 'Python', 'version': 3.7}
+Python
+3.7
