09c94c4083b93fbb5ae2e89099660aaa10d1eac9
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2022-11-16 12:19:30 +0530

    Update Tuple.py

--- Tuple in Python/Tuple.py
+++ Tuple in Python/Tuple.py
@@ -3,3 +3,10 @@ print tuple  # Prints complete list
 print tuple[0]  # Prints first element of the list
 print tuple[1:3]  # Prints elements starting from 2nd till 3rd
 print tuple[2:]  # Prints elements starting from 3rd element
+
+# Below is the Output :
+
+('Python', 123, 3.7, 'Code', 93.5)
+Python
+(123, 3.7)
+(3.7, 'Code', 93.5)
