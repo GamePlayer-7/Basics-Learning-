4707ee32c657e9e3715862bf952dd1885b3176cb
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2022-11-16 12:34:49 +0530

    Create Sets.py

---
+++ Sets in Python/Sets.py
@@ -0,0 +1,9 @@
+new_set = set(["a", "b", "c"])
+print(new_set)
+new_set.add("d")                # Adding element to the set
+print(new_set)                  # Prints set with new Element Added
+
+# Below is the Output :
+
+{'b', 'c', 'a'}
+{'b', 'c', 'd', 'a'}
