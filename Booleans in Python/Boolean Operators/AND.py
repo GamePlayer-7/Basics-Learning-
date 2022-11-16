cc01fbfd2475ff357994a1ebc5d752f3774b914f
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2022-11-16 11:41:08 +0530

    Create AND.py

---
+++ Booleans in Python/Boolean Operators/AND.py
@@ -0,0 +1,18 @@
+a = 0
+b = 2
+c = 4
+ 
+if a > b and b<c:
+    print(True)
+else:
+    print(False)
+     
+if a and b and c:
+    print("All the numbers has boolean value as True")
+else:
+    print("Atleast one number has boolean value as False")
+    
+# Below is the Output :
+
+False
+All the numbers has boolean value as True
