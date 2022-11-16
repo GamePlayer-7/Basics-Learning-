53f1b20cc81c297ab76b175315adea680abccd85
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2022-11-16 12:18:33 +0530

    Add files via upload
    tuple[2] = Program # Invalid syntax with tuple

---
+++ Tuple in Python/Tuple.py
@@ -0,0 +1,5 @@
+tuple = ('abcd', 786, 2.23, 'john', 70.2)
+print tuple  # Prints complete list
+print tuple[0]  # Prints first element of the list
+print tuple[1:3]  # Prints elements starting from 2nd till 3rd
+print tuple[2:]  # Prints elements starting from 3rd element
