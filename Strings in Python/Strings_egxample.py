41f86264823e0ae78a248361447343c385252e91
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2022-11-18 11:44:15 +0530

    Update Strings_egxample.py

--- Strings in Python/Strings_egxample.py
+++ Strings in Python/Strings_egxample.py
@@ -1,7 +1,7 @@
 string = 'Strings in Python'
 print(string)            # Prints complete string
 print(string[0])         # Prints first character of the string
-print(string[5:13])      # Prints characters starting from 3rd to 5th
+print(string[5:13])      # Prints characters starting from 6th to 12th
 print(string[3:])        # Prints string starting from 4th character
 print(string[:7])        # Prints string starting from Begning till 6th character
 print(string * 2)        # Prints string two times
