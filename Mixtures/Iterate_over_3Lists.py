40dcfbdd053a54693ebd92c07add8ea111ff93f4
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2022-11-27 12:08:32 +0530

    Create Iterate_over_3Lists.py

---
+++ Mixtures/Iterate_over_3Lists.py
@@ -0,0 +1,20 @@
+import itertools  
+
+num = [1, 2, 3] 
+
+tech = ['Python', 'Java', 'Html'] 
+
+value = [3.7, 19] 
+
+  
+# iterates over 3 lists and executes  
+# 2 times as len(value)= 2 which is the 
+# minimum among all the three  
+
+for (a, b, c) in zip(num, tech, value): 
+  print (a, b, c) 
+  
+# Below is the Output : 
+
+1 Python 3.7
+2 Java 19
