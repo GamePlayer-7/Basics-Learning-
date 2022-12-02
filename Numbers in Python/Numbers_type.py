4610b6433acb6d553c9083fd690e256602d867e3
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2022-12-02 12:20:40 +0530

    Create Numbers_type.py

---
+++ Numbers in Python/Numbers_type.py
@@ -0,0 +1,15 @@
+num1 = 3
+print(num1, 'is of type', type(num1))
+
+num2 = 7.0
+print(num2, 'is of type', type(num2))
+
+num3 = 2+5j
+print(num3, 'is of type', type(num3))
+
+
+# Below is the Output :
+
+3 is of type <class 'int'>
+7.0 is of type <class 'float'>
+(2+5j) is of type <class 'complex'>
